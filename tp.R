a93ddf72ea9036f3ed984797403d87798ae8e605
Valenchu <68815558+fasiluva@users.noreply.github.com>
2024-04-10 15:01:36 -0300

    .

--- tp.R
+++ tp.R
@@ -60,6 +60,8 @@ datos_excel_solo_resultados$C39[is.na(datos_excel_solo_resultados$C39)] <- "No"
 
 cant_viviendas <- length(datos_excel_solo_resultados[["C1"]])
 paleta <- c("#1c0b2b", "#413b6b", "#5c65c0", "#6f95ff")
+paleta_degradado <- colorRampPalette(c(paleta[4], paleta[2]))
+paleta_degradado <- paleta_degradado(10)
 fuente <- "Fuente: La Poderosa. Encuesta realizado a barrios populares de Argentina."
 
 # *********************************************************************
@@ -67,6 +69,9 @@ fuente <- "Fuente: La Poderosa. Encuesta realizado a barrios populares de Argent
 # Total de provincias
 datos_provincias <- table(datos_excel_solo_resultados[[2]])
 datos_provincias <- datos_provincias[order(datos_provincias, decreasing = TRUE)]
+# ---
+moda(datos_excel_solo_resultados[[2]])
+# ---
 
 frec_rel_prov <- round(datos_provincias/cant_viviendas * 100, 3)
 tabla_frec_prov <- t(rbind(datos_provincias, frec_rel_prov))
@@ -79,6 +84,9 @@ tabla_frec_prov
 # Total de barrios
 datos_barrios <- table(datos_excel_solo_resultados[[3]])
 datos_barrios <- datos_barrios[order(datos_barrios, decreasing=TRUE)]
+# ---
+moda(datos_excel_solo_resultados[[3]])
+# ---
 
 frec_rel_bar <-  round(datos_barrios/cant_viviendas * 100, 3)
 tabla_frec_bar <- t(rbind(datos_barrios, frec_rel_bar))
@@ -88,19 +96,41 @@ rownames(tabla_frec_bar)[24] <- "Total"
 
 tabla_frec_bar
 
-
-# ================== Descripcion grafica de RENABAP e intento de desalojos.
-# ================== Descripcion grafica de arbolado. Descripcion grafica de presion del agua
-# ================== Descripcion grafica de fuentes de energia de la vivienda. 
-
 # ================== Descripcion grafica de integrantes de la familia
-# Metricas de Edad jefe del hogar 
-relevantMetrics(datos_excel_solo_resultados[["C4"]])
 
 # Grafico de torta de cantidad de personas segun genero.
 cant_varones <- datos_excel_solo_resultados[["C8"]]
+# --- 
+media(cant_varones)
+mediana(cant_varones)
+RI(cant_varones)
+moda(cant_varones)
+cuartiles(cant_varones)
+percentiles(cant_varones, seq(0,100,10))
+desvio(cant_varones)
+# ---
+
 cant_mujeres <- datos_excel_solo_resultados[["C9"]]
+# --- 
+media(cant_mujeres)
+mediana(cant_mujeres)
+RI(cant_mujeres)
+moda(cant_mujeres)
+cuartiles(cant_mujeres)
+percentiles(cant_mujeres, seq(0,100,10))
+desvio(cant_mujeres)
+# ---
+
 cant_disidentes <- datos_excel_solo_resultados[["C10"]]
+# ---
+media(cant_disidentes)
+mediana(cant_disidentes)
+RI(cant_disidentes)
+moda(cant_disidentes)
+cuartiles(cant_disidentes)
+percentiles(cant_disidentes, seq(0,100,10))
+desvio(cant_disidentes)
+# ---
 
 cant_total_personas <- c(sum(cant_varones), sum(cant_mujeres), sum(cant_disidentes))
 options_pie <- c("Hombres", "Mujeres", "Disidentes")
@@ -115,9 +145,35 @@ mtext("Fuente: La Poderosa. Encuesta realizado a barrios populares de Argentina.
       side=1, line=1, at=-0.6)
 
 cant_menores <- datos_excel_solo_resultados[["C11"]]
+# ---
+media(cant_menores)
+mediana(cant_menores)
+RI(cant_menores)
+moda(cant_menores)
+cuartiles(cant_menores)
+percentiles(cant_menores, seq(0,100,10))
+desvio(cant_menores)
+# ---
+
 cant_discapacidad <- datos_excel_solo_resultados[["C12"]]
+# ---
+media(cant_discapacidad)
+moda(cant_discapacidad)
+cuartiles(cant_discapacidad)
+percentiles(cant_discapacidad, seq(0,100,10))
+desvio(cant_discapacidad)
+# ---
 cant_menores_total <- sum(cant_menores)
 cant_discapacidad_total <- sum(cant_discapacidad)
+
+boxplot(cant_mujeres, cant_varones, cant_disidentes, 
+        names=c("Mujeres", "Varones", "Disidentes"),
+        col=paleta[-1],
+        ylab="Cantidad de personas",
+        axes=TRUE,
+        main="Boxplot comparativo entre cantidad de mujeres, varones y disidentes en la vivienda.\nBarrios populares de Argentina, año 2020.")
+axis(2, seq(1,10,2))
+mtext(fuente, side=1, line=3, at=1)
 
 print(paste("Hay un total de", 
             cant_menores_total, 
@@ -131,11 +187,26 @@ print(paste("Hay un total de",
             round(cant_discapacidad_total/sum(cant_total_personas) * 100, 2),
             "% del total de personas."))
 
+tabla_integrantes_menores <- table(datos_excel_solo_resultados[["C6"]], cant_menores)
+plot(x=datos_excel_solo_resultados[["C6"]], y=cant_menores, 
+     type="p",
+     xlab="Cantidad de integrantes",
+     ylab="Cantidad de menores",
+     col=determinaColor(tabla_integrantes_menores, paleta_degradado),
+     )
+
+
 # ================== Descripcion grafica de obtencion de agua y saneamiento.
 
 # Como obtiene el agua?
 par(las=1, cex.lab=1.2, mar=c(5,10,5,1))
+cant_obtencion_agua <- datos_excel_solo_resultados[["C24"]]
+# ---
+moda(cant_obtencion_agua)
+# ---
+
 cant_obtencion_agua <- table(datos_excel_solo_resultados[["C24"]])
+
 barplot(height=cant_obtencion_agua[order(cant_obtencion_agua, decreasing=TRUE)],
         horiz=TRUE,
         xlim=c(0,700),
@@ -147,9 +218,16 @@ mtext("Fuente: La Poderosa. Encuesta realizado a barrios populares de Argentina.
       side=1, line=3, at=100)
 
 dev.off()
+
+cant_presion_agua <- datos_excel_solo_resultados[["C26"]]
+# ---
+mediana(presion_a_numeric(cant_presion_agua))
+moda(cant_presion_agua)
+cuartiles(presion_a_numeric(cant_presion_agua))
+# ---
+
 cant_presion_agua <- table(datos_excel_solo_resultados[["C26"]])
 cant_presion_agua_totales <- c(cant_presion_agua[[1]], cant_presion_agua[[2]], cant_presion_agua[[3]])
-
 
 pie(cant_presion_agua, 
     col= paleta,
@@ -168,26 +246,77 @@ barplot(tabla_presion_almacenamiento,
         horiz=TRUE,
         col=paleta,
         xlim=c(0,300),
+        xlab="Viviendas",
         main="Presencia de capacidad de almacenamiento de agua en altura respecto a la presión del agua.\nBarrios populares de Argentina, 2020."
         )
+axis(side=1, seq(25,275,25))
 abline(v=seq(0,300,25), col="grey", lty="dotted",)
 legend(x =170, y = 15, 
        legend=c("No", "Si, entre 200 a 500 lts", "Si, de mas de 500 lts", "Si, de menos de 200 lts"), 
        fill = paleta)
-mtext(fuente, side=1, line=3, at=100)
+mtext("Nota: Se excluyeron 2 resultados en los que afirmaban tener almacenamiento de agua en altura, pero de 
+      una capacidad desconocida.", side=1, at=65, line=4, cex=0.8)
+mtext(fuente, side=1, line=4, at=240)
+box()
 
 dev.off()
 
 # ================== Descripcion grafica de cocina y calefaccion.
 
 cant_fuentes_calefaccion <- datos_excel_solo_resultados[["C38"]]
-fuentes <- calculaFuentesCalefaccion(cant_fuentes_calefaccion)
-cant_ventilacion <- table(datos_excel_solo_resultados[["C39"]])
+# ---
+moda(cant_fuentes_calefaccion)
+# ---
 
+cant_fuentes_calefaccion <- calculaFuentesCalefaccion(cant_fuentes_calefaccion)
+cant_fuentes_calefaccion <- c("Red de gas" = cant_fuentes_calefaccion[1], 
+                              "Gas envasado" = cant_fuentes_calefaccion[2], 
+                              "Leña/carbón" = cant_fuentes_calefaccion[3], 
+                              "Electricidad" = cant_fuentes_calefaccion[4])
+cant_ventilacion <- datos_excel_solo_resultados[["C39"]]
+# ---
+moda(cant_ventilacion)
+# ---
+
+cant_ventilacion <- table(datos_excel_solo_resultados[["C39"]])
+par(mfrow = c(1, 2))
+
+barplot(cant_fuentes_calefaccion,
+        beside=TRUE,
+        col=paleta[3],
+        axes=FALSE,
+        xlab="Fuente",
+        ylab="Viviendas",
+        main="Fuentes de calefacción de las viviendas.\nBarrios populares de Argentina, 2020."
+        )
+axis(side=2, at=seq(0, 550, 50))
+abline(h=seq(0,550,50), col="grey", lty="dotted")
+box()
+
+pie(cant_ventilacion, 
+    col= paleta,
+    labels=paste(c("No", "No aplica", "Si"), c("\n"), 
+                 round(cant_ventilacion/cant_viviendas * 100, 2), c("%")), 
+    clockwise=TRUE,
+    radius=1,
+    main="Viviendas con ventilación para la calefacción.\nBarrios populares de Argentina, 2020."
+)
+
+mtext("Nota: No aplica representa las viviendas que sólo tienen calefacción mediante electricidad.", 
+      side=1, line=1, at=0, cex=0.8)
+mtext(fuente, side=1, line=4, at=0)
+box()
+
+dev.off()
 
 # ================== Descripcion grafica de alquiler.
 
 par(mfrow = c(1, 2))
+
+cant_propiedad <- datos_excel_solo_resultados[["C19"]]
+# ---
+moda(cant_propiedad)
+# ---
 
 cant_propiedad <- table(datos_excel_solo_resultados[["C19"]])
 
@@ -198,10 +327,17 @@ barplot(cant_propiedad[order(cant_propiedad, decreasing=TRUE)],
     ylab="Viviendas",
     xlab="Tenencia")
 abline(h=seq(0,600, 50), col="black", lty=3)
-mtext(fuente, side=1, line=4, at=1.5)
+box()
 
 cant_costo <- datos_excel_solo_resultados[["C21"]]
 cant_costo <- eliminaNA(cant_costo)
+# ---
+mediana(cant_costo)
+RI(cant_costo)
+cuartiles(cant_costo)
+percentiles(cant_costo, seq(0,100,10))
+rango(cant_costo)
+# ---
 
 hist(cant_costo,
      breaks=seq(3000, 30000, 3000),
@@ -214,7 +350,8 @@ hist(cant_costo,
 axis(side=1, at=seq(3000, 30000, 3000))
 axis(side=2, at=seq(0, 40, 5))
 abline(h=seq(5, 35, 5), col="black", lty=3)
-mtext(fuente, side=1, line=4, at=5000)
+mtext(fuente, side=1, line=4, at=100)
+box()
 
 dev.off()
 
@@ -222,8 +359,22 @@ dev.off()
 
 par(mfrow = c(1, 2))
 
-cant_tendido_electrico <- table(datos_excel_solo_resultados[["C41"]])
-cant_incendios <- table(datos_excel_solo_resultados[["C43"]])
+cant_tendido_electrico <- datos_excel_solo_resultados[["C41"]]
+# ---
+mediana(tendido_a_numeric(cant_tendido_electrico))
+moda(cant_tendido_electrico)
+cuartiles(tendido_a_numeric(cant_tendido_electrico))
+# ---
+
+cant_tendido_electrico <- table(cant_tendido_electrico)
+cant_incendios <- datos_excel_solo_resultados[["C43"]]
+# ---
+mediana(incendio_a_numeric(cant_incendios))
+moda(cant_incendios)
+cuartiles(incendio_a_numeric(cant_incendios))
+# ---
+
+cant_incendios <- table(cant_incendios)
 
 pie(cant_tendido_electrico, 
     col= paleta,
@@ -233,7 +384,6 @@ pie(cant_tendido_electrico,
     radius=1.2,
     main="Tendido eléctrico de las viviendas.\nBarrios populares de Argentina, 2020."
 )
-mtext(fuente, side=1, line=2)
 
 pie(cant_incendios, 
     col= c(paleta[2], paleta[1]),
@@ -243,13 +393,17 @@ pie(cant_incendios,
     radius=1.2,
     main="Viviendas que experimentaron incendios en el último año\n debido a las condiciones de la instalacíon eléctrica.\nBarrios populares de Argentina, 2020."
 )
-mtext(fuente, side=1, line=2)
+mtext(fuente, side=1, line=2, at=-2.5)
 
 dev.off()
 
 # ================== Descripcion grafica de conectividad.
 
 cant_conectividad <- datos_excel_solo_resultados[["C46"]]
+# ---
+moda(cant_conectividad)
+# ---
+
 cant_conectividad <- tieneConectividad(cant_conectividad, datos_excel_solo_resultados[["C47"]])
 
 pie(cant_conectividad, 
@@ -257,16 +411,68 @@ pie(cant_conectividad,
     labels=paste(c("Si", "No"), c("\n"), 
                  round(cant_conectividad/cant_viviendas * 100, 2), c("%")), 
     clockwise=TRUE,
-    radius=1,
+    radius=0.9,
     main="Viviendas con al menos un teléfono celular con conectividad a Internet.\nBarrios populares de Argentina, 2020."
 )
 mtext(fuente, side=1, line=2)
 
+# ================== Descripcion grafica de filtraciones y riesgo de derrumbe.
 
+par(mfrow = c(1, 2))
 
-# ================== Medidas de posicion y dispersion de anteriores mencionados
-# ================== Relacion entre 2 variables?
+cant_filtraciones <- datos_excel_solo_resultados[["C63"]]
+# ---
+moda(cant_filtraciones)
+# ---
+cant_filtraciones <- calculaFiltraciones(cant_filtraciones)
 
+barplot(cant_filtraciones[order(cant_filtraciones, decreasing=FALSE)], 
+        col=paleta[3],
+        ylim=c(0, 600), 
+        main="Filtraciones/humedad en las viviendas\nBarrios populares de Argentina, 2020.",
+        ylab="Viviendas",
+        xlab="Ubicación de la filtración")
+abline(h=seq(0,600, 50), col="black", lty=3)
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+cant_riesgo_derrumbe <- datos_excel_solo_resultados[["C64"]]
+# ---
+moda(cant_riesgo_derrumbe)
+mediana(riesgo_a_numeric(cant_riesgo_derrumbe))
+cuartiles(riesgo_a_numeric(cant_riesgo_derrumbe))
+# ---
+
+cant_riesgo_derrumbe <- calculaRiesgoDerrumbe(cant_riesgo_derrumbe)
+
+pie(cant_riesgo_derrumbe, 
+    col= c(paleta[2], paleta[1]),
+    labels=paste(c("Con riesgo", "Sin riesgo"), c("\n"), 
+                 round(cant_riesgo_derrumbe/cant_viviendas * 100, 2), c("%")), 
+    clockwise=TRUE,
+    radius=1,
+    main="Viviendas con problemas estructurales graves y riesgo de derrumbe\nBarrios populares de Argentina, 2020."
+)
+mtext(fuente, side=1, line=2)
+
+dev.off()
+
+# ================== Descripcion grafica de plagas.
+
+cant_plagas <- datos_excel_solo_resultados[["C73"]]
+# ---
+moda(cant_plagas)
+# ---
+
+cant_plagas <- calculaPlagas(cant_plagas)
+
+barplot(cant_plagas[order(cant_plagas, decreasing=FALSE)], 
+        col=paleta[3],
+        ylim=c(0, 600), 
+        main="Plagas presentes en las viviendas\nBarrios populares de Argentina, 2020.",
+        xlab="Plagas",
+        ylab="Cantidad de viviendas con dicha plaga")
+axis(2, seq(50, 550, 50))
+abline(h=seq(0,600, 50), col="black", lty=3)
+box()
+mtext(fuente, side=1, line=4, at=1)
+
 
--- tp_functions.R
+++ tp_functions.R
@@ -1,9 +1,10 @@
-Moda <- function(x) {
+moda <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
 
 RI <- function(x) {
+  x <- x[order(x)]
   cuartil1_index <- (length(x) + 1) / 4
   cuartil3_index <- cuartil1_index * 3
   
@@ -19,6 +20,36 @@ RI <- function(x) {
   }
   
   return(cuartil3 - cuartil1)
+}
+
+cuartiles <- function(x) {
+  cuartiles <- quantile(x, probs=c(0.25, 0.50, 0.75))
+  return(cuartiles)
+}
+
+percentiles <- function(x, division) {
+  percentiles <- quantile(x, probs= division / 100)
+  return(percentiles)
+}
+
+rango <- function(x) {
+  return(max(x) - min(x))
+}
+
+media <- function(x) {
+  return(round(mean(x), 3))
+}
+
+mediana <- function(x) {
+  return(median(x))
+}
+
+variacion <- function(x) {
+  return(round(var(x), 3))
+}
+
+desvio <- function(x) {
+  return(round(sd(x), 3))
 }
 
 relevantMetrics <- function(x) {
@@ -46,14 +77,8 @@ relevantMetrics <- function(x) {
   print(paste("Variacion:", variacion))
   print(paste("Desvio estandar:", desvio))
   
-  # Doble grafico
-  #par(mfrow = c(1, 2))
-  
-  # Grafico de dispersion.
-  #plot(x, ylab="Values", main="Grafico de dispersion")
-  
   # Boxplot.
-  boxplot(x, ylab="Values", main="Boxplot")
+  boxplot(x, ylab="Values", main="Boxplot de la variable")
   grid()
   
   readline()
@@ -145,5 +170,159 @@ tieneConectividad <- function(servicio, datos) {
   }
   
   return(c(si, no))
+}
+
+calculaFiltraciones <- function(datos) {
+  
+  banio <- 0
+  cocina <- 0
+  living <- 0
+  dormitorios <- 0
+  otro <- 0
+  ninguno <- 0
+  
+  for (i in 1:length(datos)) {
+    if (grepl("Dormitorios", datos[i])){
+      dormitorios <- dormitorios + 1
+    }
+    
+    if (grepl("Cocina", datos[i])) {
+      cocina <- cocina + 1
+    }
+    
+    if (grepl("Baño", datos[i])) {
+      banio <- banio + 1
+    }
+    
+    if (grepl("Living", datos[i])) {
+      living <- living + 1
+    }
+    
+    if (grepl("Otro", datos[i])) {
+      otro <- otro + 1
+    }
+    
+    if (grepl("No hay", datos[i])) {
+      ninguno <- ninguno + 1
+    }
+  }
+  
+  return(c("Baño" = banio, 
+         "Cocina" = cocina, 
+         "Living" = living, 
+         "Otro" = otro, 
+         "Dormitorios" = dormitorios,
+         "Ninguna filtración" = ninguno))
+}
+
+calculaRiesgoDerrumbe <- function(datos) {
+  
+  si <- 0
+  no <- 0
+  
+  for (i in 1:length(datos)) {
+    if (grepl("No hay", datos[i])) {
+      no <- no + 1
+    }
+    else {
+      si <- si + 1
+    }
+  }
+  
+  return(c("Con riesgo" = si, "Sin riesgo" = no))
+}
+
+calculaPlagas <- function(datos) {
+  
+  cucarachas <- 0
+  ratas <- 0
+  mosquitos <- 0
+  nada <- 0
+  
+  for (i in 1:length(datos)) {
+    if (datos[i] == "") {
+      nada <- nada + 1
+    }
+    if (grepl("Ratas", datos[i])) {
+      ratas <- ratas + 1
+    }
+    
+    if (grepl("Mosquitos", datos[i])) {
+      mosquitos <- mosquitos + 1
+    }
+    
+    if (grepl("Cucarachas", datos[i])) {
+      cucarachas <- cucarachas + 1
+    }
+    
+  }
+  
+  return(c("Cucarachas" = cucarachas, "Mosquitos" = mosquitos, 
+           "Ratas" = ratas, "Ninguna" = nada))
+}
+
+tendido_a_numeric <- function(x) {
+  res <- c()
+  
+  for (i in 1:length(x)) {
+    if (grepl("Todo", x[i])) {
+      res <- c(res, 2)
+    }
+    else {
+      res <- c(res, 1)
+    }
+  }
+  
+  return(res)
+}
+
+incendio_a_numeric <- function(x) {
+  res <- c()
+  
+  for (i in 1:length(x)) {
+    if (grepl("Si", x[i])) {
+      res <- c(res, 2)
+    }
+    else {
+      res <- c(res, 1)
+    }
+  }
+  
+  return(res)
+}
+
+riesgo_a_numeric <- function(x) {
+  res <- c()
+  
+  for (i in 1:length(x)) {
+    if (grepl("Con", x[i])) {
+      res <- c(res, 1)
+    }
+    else {
+      res <- c(res, 2)
+    }
+  }
+  
+  return(res)
+}
+
+presion_a_numeric <- function(x) {
+  
+  res <- c()
+  
+  for (i in 1:length(x)) {
+    if (grepl("Buena", x[i])) {
+      res <- c(res, 3)
+    }
+    else if (grepl("Muy", x[i])) {
+      res <- c(res, 1)
+    }
+    else {
+      res <- c(res, 2)
+    }
+  }
+  
+  return(res)
+  
 }
 
